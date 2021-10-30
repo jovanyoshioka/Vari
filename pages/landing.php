f8c633dbbf380849c5c013eac7f4588b41181d26
Jovan Yoshioka <jovanyoshioka@gmail.com>
2021-10-30 00:30:03 -0400

    Created file structure.

---
+++ pages/landing.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Schedule-App</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Schedule web-based application for UTK VolHacks V hackathon.">
+    <meta name="keywords" content="Schedule, App, UTK, Jovan, Jovi, Yoshioka, Dong, Jun, Woun, Meet, Patel">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/global.css">
+    <link rel="stylesheet" type="text/css" href="../css/landing.css">
+  </head>
+  <body>
+
+    <h1>Hello World!</h1>
+
+    <script src="../js/main.js"></script>
+    <script src="../js/landing.js"></script>
+  </body>
+</html>
